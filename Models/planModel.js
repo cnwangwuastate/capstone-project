1dff0378874b6f9b8eda7b118c17ffeccbe5ce52
Bradley Hale <bradh2018@gmail.com>
2024-03-29 12:01:00 -0500

    pushing so project members have something to pull

--- Models/planModel.js
+++ Models/planModel.js
@@ -1,6 +1,149 @@
 "use strict";
 const db = require("./db");
 
+function generalSearch(params, searchOperations = []) {
+    let { SQFTLower, SQFTUpper, lengthUpper, lengthLower, widthUpper, widthLower, HeightUpper, HeightLower, floors} = params;
+    let conditions = [];
+    let args = {};
+    console.log(params);
+    
+    // Add conditions based on search operations
+    searchOperations.forEach(operation => {
+        switch (operation) {
+            case 'searchBySQFT':
+                if (SQFTLower !== undefined && SQFTUpper !== undefined) {
+                    conditions.push(`overallSQF <= @SQFTUpper`);
+                    conditions.push(`overallSQF >= @SQFTLower`);
+                    args['@SQFTLower'] = SQFTLower;
+                    args['@SQFTUpper'] = SQFTUpper;
+                }
+                break;
+            case 'searchByWidth':
+                if (widthLower !== undefined && widthUpper !== undefined) {
+                    conditions.push(`width <= @widthUpper`);
+                    conditions.push(`width >= @widthLower`);
+                    args['@widthLower'] = widthLower;
+                    args['@widthUpper'] = widthUpper;
+                }
+                break;
+            case 'searchByLength':
+                if (lengthLower !== undefined && lengthUpper !== undefined) {
+                    conditions.push(`length <= @lengthUpper`);
+                    conditions.push(`length >= @lengthLower`);
+                    args['@lengthLower'] = lengthLower;
+                    args['@lengthUpper'] = lengthUpper;
+                }
+                break;
+            case 'searchByHeight':
+                if (HeightLower !== undefined && HeightUpper !== undefined) {
+                    conditions.push(`Height <= @HeightUpper`);
+                    conditions.push(`Height >= @HeightLower`);
+                    args['@HeightLower'] = HeightLower;
+                    args['@HeightUpper'] = HeightUpper;
+                }
+                break;
+
+            case 'searchByFloors':
+                if (floors !== undefined) {
+                    conditions.push(`stories = @floors`);
+                    args['@floors'] = floors;
+                }
+                break;
+        }
+    });
+
+
+    // Construct the WHERE clause
+    let whereClause = '';
+    if (conditions.length > 0) {
+        whereClause = 'WHERE ' + conditions.join(' AND ');
+    }
+
+    // Prepare and execute SQL query
+    const sqlQuery = `BEGIN TRANSACTION;
+    SELECT * FROM plans ${whereClause}
+    END TRANSACTION`;
+    const stmt = db.prepare(sqlQuery);
+    const result = stmt.all(args);
+
+    return result;
+}
+
+/*
+    MODIFY DATABASE:
+    INSERT INTO plans(columns) VALUES (values)
+    how do you go about doing this?
+    a similar structure to the add function, however: each column should be input into a "columns" string,
+    and each value being inserted into a "values" string
+    once those strings are complete, do a similar loop for the columns and values clauses
+
+    let columnClause = '';
+    if(columns.length > 0) {
+        columnClause = 'plans(' + columns.join() + ')');
+    }
+
+    let valueClause = '';
+    if (values.length > 0) {
+        valueClause = 'VALUES(' + values.join() + ')');
+    }
+*/
+
+function editPlan(planID, editParams) {
+    let setStatements = [];
+    let args = { "@planID": planID };
+
+    // Add conditions based on edit parameters
+    Object.entries(editParams).forEach(([key, value]) => {
+        switch (key) {
+            case 'overallSQFT':
+                setStatements.push(`overallSQFT = @overallSQFT`);
+                args['@overallSQFT'] = value;
+                break;
+            case 'lengthFt':
+                setStatements.push(`lengthFt = @lengthFt`);
+                args['@lengthFt'] = value;
+                break;
+            case 'lengthIn':
+                setStatements.push(`lengthIn = @lengthIn`);
+                args['@lengthIn'] = value;
+                break;
+            case 'widthFt':
+                setStatements.push(`widthFt = @widthFt`);
+                args['@widthFt'] = value;
+                break;
+            case 'widthIn':
+                setStatements.push(`widthIn = @widthIn`);
+                args['@widthIn'] = value;
+                break;
+            case 'heightFt':
+                setStatements.push(`heightFt = @heightFt`);
+                args['@heightFt'] = value;
+                break;
+            case 'heightIn':
+                setStatements.push(`heightIn = @heightIn`);
+                args['@heightIn'] = value;
+                break;
+            case 'floors':
+                setStatements.push(`floors = @floors`);
+                args['@floors'] = value;
+                break;
+        }
+    });
+
+    // Construct the SET clause
+    let setClause = '';
+    if (setStatements.length > 0) {
+        setClause = 'SET ' + setStatements.join(', ');
+    }
+
+    // Prepare and execute SQL query
+    const sqlQuery = `BEGIN TRANSACTION;
+    UPDATE plans ${setClause} WHERE planID = @planID
+    END TRANSACTION`;
+    const stmt = db.prepare(sqlQuery);
+    stmt.run(args);
+}
+
 function testFunction() {
     const sql = `SELECT * FROM plans`;
     const stmt = db.prepare(sql);
@@ -8,15 +151,14 @@ function testFunction() {
     return result;
 }
 
-function searchByLength(upper, lower) {
-    const sql = `SELECT * FROM plans WHERE lengthFt BETWEEN @upper AND @lower`;
+function deletePlan(planID) {
+    const sql = `BEGIN TRANSACTION;
+    DELETE FROM plans WHERE planID=@planID;
+    END TRANSACTION;`;
     const stmt = db.prepare(sql);
-    const result = stmt.all({
-        "upper":upper,
-        "lower":lower
+    stmt.run({
+        "@planID": planID
     });
-    return result;
-
 }
 
 function searchByWidth(upper, lower) {
@@ -70,11 +212,14 @@ function getPlanByID(planID) {
 }
 
 module.exports = {
+    generalSearch,
     testFunction,
     searchByFloors,
-    searchByLength,
     searchByWidth,
     searchBySQFT,
     getPlanByID,
-    searchByHeight
+    searchByHeight,
+    generalSearch,
+    deletePlan,
+    editPlan
 }
